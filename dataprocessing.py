09de719c600b72834c7d8967674b5fc1eb63c22b
yutteee <yusaku_nakamura_aa@icloud.com>
2022-09-26 03:32:36 +0900

    変換しても文字数が同じものを削除 #1

--- dataprocessing.py
+++ dataprocessing.py
@@ -4,4 +4,8 @@ df = pd.read_csv("OriginalWordSimilarityDataset/score_noun.csv")
 feature_selection = ['word1', 'word2', 'mean']
 
 dataset = df[feature_selection]
-print(dataset)
+# word1, word2の文字数が同じ行を削除
+word1Length = dataset['word1'].str.len()
+word2Length = dataset['word2'].str.len()
+dataset = dataset[word1Length != word2Length]
+# 1列目が文字数の小さい方、2列目が文字数の大きい方にする
