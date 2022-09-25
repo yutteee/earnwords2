7b3cae64ce484b1c419943773c6be2bf396c49cb
yutteee <yusaku_nakamura_aa@icloud.com>
2022-09-26 04:11:22 +0900

    1列目が少ない文字数になるようにした #1

--- dataprocessing.py
+++ dataprocessing.py
@@ -4,8 +4,15 @@ df = pd.read_csv("OriginalWordSimilarityDataset/score_noun.csv")
 feature_selection = ['word1', 'word2', 'mean']
 
 dataset = df[feature_selection]
+
 # word1, word2の文字数が同じ行を削除
 word1Length = dataset['word1'].str.len()
 word2Length = dataset['word2'].str.len()
 dataset = dataset[word1Length != word2Length]
+
 # 1列目が文字数の小さい方、2列目が文字数の大きい方にする
+smallWord1LengthData = dataset[word1Length < word2Length]
+bigWord1LengthData = dataset[word1Length > word2Length]
+bigWord1LengthData = bigWord1LengthData[['word2', 'word1', 'mean']]
+bigWord1LengthData = pd.DataFrame(data=bigWord1LengthData.values, columns=feature_selection)
+dataset = pd.concat([smallWord1LengthData, bigWord1LengthData], axis=0)
